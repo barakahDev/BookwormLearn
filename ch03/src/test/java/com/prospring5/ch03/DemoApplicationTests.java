293e5ef1bd54e95792a0143464296fcb85bd67ee
barakah <barakah.noor@outlook.com>
2020-06-22 06:43:31 +0700

    First Commit

---
+++ ch03/src/test/java/com/prospring5/ch03/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prospring5.ch03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
